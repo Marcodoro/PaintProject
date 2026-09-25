7c1e0c33007f0e72f9de663f7f24676ff8fccccf
Marco Dori <marcodori2010@gmail.com>
2026-09-25 20:18:39 +0200

    Optimization update button array

--- src/Game.java
+++ src/Game.java
@@ -7,40 +7,37 @@ public class Game {
     private Buntstift pen;
     private Tastatur key;
     private Maus mouse;
-    private Button yellow;
-    private Button green;
-    private Button blue;
-    private Button orange;
-    private Button pink;
-    private Button radieren;
+    private Button[] buttons;
+
 
     public Game() {
         screen = new Bildschirm(1280, 720);
         pen = new Buntstift();
         key = new Tastatur();
         mouse = new Maus();
-        yellow = new Button(100, 100, 100, 100, Farbe.GELB);
-        green = new Button(300, 100, 100, 100, Farbe.GRUEN);
-        blue = new Button(500, 100, 100, 100, Farbe.BLAU);
-        orange = new Button(700, 100, 100, 100, Farbe.ORANGE);
-        pink = new Button(900, 100, 100, 100, Farbe.PINK);
-        radieren = new Button(1100, 100, 100, 100, Farbe.SCHWARZ);
+
+        int[] farben = {
+            Farbe.GELB, Farbe.BLAU, Farbe.GRUEN, Farbe.ORANGE, Farbe.PINK, Farbe.SCHWARZ
+        };
+
+        buttons = new Button[farben.length];
 
         pen.setzeLinienBreite(10);
         pen.setzeFarbe(Color.BLACK);
+
+        for (int i = 0; i < buttons.length; i++) {
+            int xPosition = 100 + (i * 200);
+            buttons[i] = new Button(xPosition, 100, 100, 100, farben[i]);
+        }
     }
     public void start() {
         erstelleButtons();
         malen();
     }
     public void erstelleButtons() {
-        yellow.drawButton(pen);
-        green.drawButton(pen);
-        blue.drawButton(pen);
-        orange.drawButton(pen);
-        pink.drawButton(pen);
-        radieren.drawButton(pen);
-
+        for (Button p: buttons) {
+            p.drawButton(pen);
+        }
     }
     public void checkCollisionButtons(Button buttons) {
         int mouseX = mouse.hPosition();
@@ -67,13 +64,10 @@ public class Game {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            checkCollisionButtons(yellow);
-            checkCollisionButtons(green);
-            checkCollisionButtons(blue);
-            checkCollisionButtons(pink);
-            checkCollisionButtons(orange);
-            checkCollisionButtons(radieren);
 
+            for (Button p : buttons) {
+                checkCollisionButtons(p);
+            }
 
             if (mouse.istGedrueckt()) {
                 int mouseX = mouse.hPosition();
